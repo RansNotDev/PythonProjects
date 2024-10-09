27f26891c89f9db980f8758dc0cc855b66edde4f
RansNotDev <ranyboytemplado001@gmail.com>
2024-10-09 08:34:23 +0800

    Greetings and birthday

--- Shopping_Cart.py
+++ Shopping_Cart.py
@@ -2,4 +2,4 @@ item = input("What Item would you like to buy?: ")
 price = float(input("What is the price? : "))
 quantity = int(input("How many would you like? : "))
 total = price * quantity
-print(f"Item Name: {item}\nquantity: {quantity} \nTotal Amount to Pay: {total}")
+print(f"Item Name: {item}\nquantity: {quantity} \nTotal Amount to Pay: ₱{total}")
---
+++ greetings.py
@@ -0,0 +1,6 @@
+#Calculate Age using Birth Year
+Name = input("Enter Name : ")
+birthyear = int(input("Enter Birth year: "))
+year_today = 2024
+yourAge = year_today - birthyear
+print(f"Hello {Name} your Age is : {yourAge}")
